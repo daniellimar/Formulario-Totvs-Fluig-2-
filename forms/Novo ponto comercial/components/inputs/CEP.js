712b24e426da7cb6cfc14ba08177e26bdf3ded91
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2024-08-09 11:47:51 -0400

    Refactoring components\inputs\CEP

--- forms/Novo ponto comercial/components/inputs/CEP.js
+++ forms/Novo ponto comercial/components/inputs/CEP.js
@@ -1,22 +1,37 @@
-function inputCEP(name = 'cep', id = 'cep', label = 'CEP', placeholder = 'Informe o CEP', value = '', type = 'text') {
-    // Gera o HTML do input
+function inputCEP(options = {}) {
+    const defaults = {
+        name: 'cep', id: 'cep', label: 'CEP', placeholder: 'Informe o CEP', value: '', type: 'text', divPai: ''
+    };
+
+    const settings = {...defaults, ...options};
+
     const inputHtml = `
-        <label for="${id}">${label}</label>
-        <input type="${type}" name="${name}" id="${id}" class="form-control" value="${value}" 
-               placeholder="${placeholder}">
+        <label for="${settings.id}">${settings.label}</label>
+        <input type="${settings.type}" name="${settings.name}" id="${settings.id}" class="form-control" value="${settings.value}" 
+               placeholder="${settings.placeholder}">
     `;
 
-    // Adiciona o HTML ao contêiner e aplica a máscara
     $(document).ready(function () {
-        $(`#${id}`).mask('00000-000');
+        $(`#${settings.id}`).mask('00000-000');
 
-        // Evento de blur para buscar dados via CEP
-        $(`#${id}`).blur(function () {
+        $(`#${settings.id}`).blur(function () {
+            // Mostrar mensagem de carregamento
+            const myLoading2 = FLUIGC.loading(`#${settings.divPai}`);
+            myLoading2.show();
+
             $.getJSON('//viacep.com.br/ws/' + $(this).val() + '/json/', function (dados) {
                 $('#logradouro').val(dados.logradouro);
                 $('#bairro').val(dados.bairro);
                 $('#cidade').val(dados.localidade);
                 $('#estado').val(dados.uf);
+
+                setTimeout(() => {
+                    myLoading2.hide();
+                }, 500);
+            }).fail(function () {
+                setTimeout(() => {
+                    myLoading2.hide();
+                }, 500);
             });
         });
     });

8633b43200c47caad6e419cc254cfc510ec7670d
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2024-08-12 16:28:41 -0400

    Feature add forms\Cadastro de Pessoas

---
+++ forms/Cadastro de Pessoas/events/validateForm.js
@@ -0,0 +1,17 @@
+function validateForm(form) {
+    const requiredFields = [{
+        field: "nome", message: "Preencha o campo nome."
+    }, {
+        field: "setor", message: "Preencha o campo setor."
+    }];
+
+    for (let i = 0; i < requiredFields.length; ++i) {
+        if (valueIsEmpty(form.getValue(requiredFields[i].field))) {
+            throw requiredFields[i].message;
+        }
+    }
+}
+
+function valueIsEmpty(value) {
+    return value == null || value == "";
+}
